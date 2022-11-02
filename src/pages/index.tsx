d994f22daf3db8843e5c9500f7570c1ff86bfe7a
Willi4nn <willianpereirasilva69@gmail.com>
2022-11-02 14:49:07 -0300

    Static previews

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,13 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
+
 import styles from "./home.module.scss";
 
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   };
 }
 
@@ -17,18 +18,20 @@ export default function Home({ product }: HomeProps) {
       <Head>
         <title>Home | ig.news</title>
       </Head>
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
           <h1>
-            News about <br /> the <span>React</span> world
+            News about the <span>React</span> world.
           </h1>
           <p>
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
+
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
